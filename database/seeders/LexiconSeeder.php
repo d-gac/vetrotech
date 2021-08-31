bdf4c8319bad7d86b6e042f638ed232f3fa60b68
d-gac <darekgac1998@gmail.com>
2021-08-31 13:18:00 +0200

    version: 1.06

--- database/seeders/LexiconSeeder.php
+++ database/seeders/LexiconSeeder.php
@@ -18,97 +18,97 @@ class LexiconSeeder extends Seeder
         DB::table('lexicons')->insert([
             'name' => 'Wewnętrzne',
             'type' => 'dimensions',
-            'code_id' => 0,
+            'code_id' => 1,
             'status' => 1
         ]);
         DB::table('lexicons')->insert([
             'name' => 'Zewnętrzne',
             'type' => 'dimensions',
-            'code_id' => 1,
+            'code_id' => 2,
             'status' => 1
         ]);
         DB::table('lexicons')->insert([
             'name' => '33.1 mleczne',
             'type' => 'typeOfGlass',
-            'code_id' => 0,
+            'code_id' => 1,
             'status' => 1
         ]);
         DB::table('lexicons')->insert([
             'name' => '44.2 mleczna',
             'type' => 'typeOfGlass',
-            'code_id' => 1,
+            'code_id' => 2,
             'status' => 1
         ]);
         DB::table('lexicons')->insert([
             'name' => 'antisol',
             'type' => 'typeOfGlass',
-            'code_id' => 2,
+            'code_id' => 3,
             'status' => 1
         ]);
         DB::table('lexicons')->insert([
             'name' => 'lustro weneckie',
             'type' => 'typeOfGlass',
-            'code_id' => 3,
+            'code_id' => 4,
             'status' => 1
         ]);
         DB::table('lexicons')->insert([
             'name' => 'P2/float',
             'type' => 'typeOfGlass',
-            'code_id' => 4,
+            'code_id' => 5,
             'status' => 1
         ]);
         DB::table('lexicons')->insert([
             'name' => 'refleks',
             'type' => 'typeOfGlass',
-            'code_id' => 5,
+            'code_id' => 6,
             'status' => 1
         ]);
         DB::table('lexicons')->insert([
             'name' => 'duplex',
             'type' => 'nameOfGlass',
-            'code_id' => 0,
+            'code_id' => 1,
             'status' => 1
         ]);
         DB::table('lexicons')->insert([
             'name' => 'Ernesto',
             'type' => 'nameOfGlass',
-            'code_id' => 1,
+            'code_id' => 2,
             'status' => 1
         ]);
         DB::table('lexicons')->insert([
             'name' => 'naświetle boczne',
             'type' => 'nameOfGlass',
-            'code_id' => 2,
+            'code_id' => 3,
             'status' => 1
         ]);
         DB::table('lexicons')->insert([
             'name' => 'szlif',
             'type' => 'nameOfGlass',
-            'code_id' => 3,
+            'code_id' => 4,
             'status' => 1
         ]);
         DB::table('lexicons')->insert([
             'name' => 'Dział Nr 1',
             'type' => 'numberDepartment',
-            'code_id' => 0,
+            'code_id' => 1,
             'status' => 1
         ]);
         DB::table('lexicons')->insert([
             'name' => 'Dział Nr 2',
             'type' => 'numberDepartment',
-            'code_id' => 1,
+            'code_id' => 2,
             'status' => 1
         ]);
         DB::table('lexicons')->insert([
             'name' => 'Osoba fizyczna',
             'type' => 'contractorType',
-            'code_id' => 0,
+            'code_id' => 1,
             'status' => 1
         ]);
         DB::table('lexicons')->insert([
             'name' => 'Osoba prawna',
             'type' => 'contractorType',
-            'code_id' => 1,
+            'code_id' => 2,
             'status' => 1
         ]);
     }
